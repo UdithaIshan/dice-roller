c4dbdf52df60ead4d29779d008228ac73ba8a570
UdithaIshan <uditha.ishan34@gmail.com>
2020-10-13 10:08:56 +0530

    Rolling functionality added

--- app/src/main/java/com/byteengine/diceapp/MainActivity.java
+++ app/src/main/java/com/byteengine/diceapp/MainActivity.java
@@ -1,8 +1,13 @@
 package com.byteengine.diceapp;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView img1 = findViewById(R.id.img1);
+        final ImageView img2 = findViewById(R.id.img2);
+        Button btn = findViewById(R.id.btnRoller);
+
+        final Random number = new Random();
+
+        final int[] imgArray = {R.drawable.dice1, R.drawable.dice2, R.drawable.dice3, R.drawable.dice4, R.drawable.dice5, R.drawable.dice6};
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final int randomNum1 = number.nextInt(6);
+                final int randomNum2 = number.nextInt(6);
+                img1.setImageResource(imgArray[randomNum1]);
+                img2.setImageResource(imgArray[randomNum2]);
+
+            }
+        });
+
+
     }
 }
